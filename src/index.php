07049d4394478c63d23f08ada9589dda9e796177
Madhukar Reddy <madhu@madhukarreddy.com>
2024-10-29 07:34:01 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -12,8 +12,10 @@ try {
     $pdo = new PDO("mysql:host=$host", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Create the database if it doesn't exist
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS `$dbname`");
     // Use the database
-    $pdo->exec("USE $dbname");
+    $pdo->exec("USE `$dbname`");
 
     // Create the users table for authentication
     $createUsersTable = "
